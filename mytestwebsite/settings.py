582f8c918f9f5846df6c01684a004e717ff8b739
Razz Sawhoney <rajsahani1819@gmail.com>
2020-05-25 22:20:11 +0545

    Update settings.py

--- mytestwebsite/settings.py
+++ mytestwebsite/settings.py
@@ -230,7 +230,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 db_from_env = dj_database_url.config(conn_max_age=500)
