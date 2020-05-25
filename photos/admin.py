31c0886802eac5266ee77e45d2fbe0307e35b914
Razz Sawhoney <rajsahani1819@gmail.com>
2020-05-26 01:36:21 +0545

    some changes

--- photos/admin.py
+++ photos/admin.py
@@ -6,6 +6,6 @@ from .models import Photo
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
 
-    fields = ['file', 'image_tag', ]
+    fields = ['file',]
     # readonly_fields = ['image_tag']
     list_display = ['file', 'image_tag', ]
