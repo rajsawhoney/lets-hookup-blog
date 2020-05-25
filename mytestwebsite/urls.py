cb6dd17a7a698c938cb545c840e9ead16e65402e
Razz Sawhoney <rajsahani1819@gmail.com>
2020-05-26 00:10:40 +0545

    some changes

--- mytestwebsite/urls.py
+++ mytestwebsite/urls.py
@@ -1,18 +1,3 @@
-"""mytestwebsite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
@@ -23,10 +8,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from dajaxice.core import dajaxice_autodiscover, dajaxice_config
+# from dajaxice.core import dajaxice_autodiscover, dajaxice_config
 from django.views.generic import TemplateView
 
-dajaxice_autodiscover()
+# dajaxice_autodiscover()
 
 urlpatterns = [
 
@@ -38,13 +23,12 @@ urlpatterns = [
     url(r'^accounts/', include('accounts.urls')),
     url(r'^article_search/', include('search.urls')),
     url(r'^tinymce/', include('tinymce.urls')),
-    url(r'^dajaxice/', include('dajaxice.urls')),
+    #     url(r'^dajaxice/', include('dajaxice.urls')),
     url(r'^photo-upload/', include('photos.urls')),
     url(r'^test/', include('checkapp.urls')),
     url(r'^$', TemplateView.as_view(
         template_name="index.html"), name='home'),
     url(r'^', include('django.contrib.auth.urls')),
-
 
 ]
 
