81482c2e8187c51e6c7746905632be5af27be15b
Razz Sawhoney <rajsahani1819@gmail.com>
2020-06-24 14:22:44 +0545

    channges

--- mytestwebsite/pipeline.py
+++ mytestwebsite/pipeline.py
@@ -4,35 +4,34 @@ from accounts.models import UserModel
 def save_profile(backend, user, response, *args, **kwargs):
     print("Responses grabed...from..", str(backend), user)
     print(response)
-
-    if 'github' in str(backend):
-        UserModel.objects.create(
-            user=user,
-            about_me=response['bio'],
-            profile_pic=response['avatar_url']
+    if user not in User.objects.all():
+        if 'github' in str(backend):
+            UserModel.objects.create(
+                user=user,
+                about_me=response['bio'],
+                profile_pic=response['avatar_url']
             )
-        print("GitHub User created!!")
-    elif 'linkedin' in str(backend):
-        UserModel.objects.create(
-            user=user,
-            profile_pic=response['profilePicture']['displayImage']
+            print("GitHub User created!!")
+        elif 'linkedin' in str(backend):
+            UserModel.objects.create(
+                user=user,
+                profile_pic=response['profilePicture']['displayImage']
             )
-        print("linkedin User created!!")
+            print("linkedin User created!!")
 
-
-
-    elif 'twitter' in str(backend):
-        UserModel.objects.create(
-            user=user,
-            about_me=response['description'],
-            profile_pic=response['profile_banner_url']
+        elif 'twitter' in str(backend):
+            UserModel.objects.create(
+                user=user,
+                about_me=response['description'],
+                profile_pic=response['profile_banner_url']
             )
-        print("twitter User created!!")
+            print("twitter User created!!")
 
-
-    elif 'facebook' in str(backend):
-        UserModel.objects.create(
-            user=user,
-            profile_pic=response['picture']['data']['url']
+        elif 'facebook' in str(backend):
+            UserModel.objects.create(
+                user=user,
+                profile_pic=response['picture']['data']['url']
             )
-        print("facebook User created!!")
+            print("facebook User created!!")
+    else:
+        print(user, "This user already exists!!!")
