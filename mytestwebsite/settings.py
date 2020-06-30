ed9be028f7e594c357e9a6b4a19ee53404842325
Razz Sawhoney <rajsahani1819@gmail.com>
2020-07-01 01:33:41 +0545

    'chna'

--- mytestwebsite/settings.py
+++ mytestwebsite/settings.py
@@ -249,7 +249,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Email Confs
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 25
