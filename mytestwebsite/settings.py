34daf8c076ef63646bad5d5bb7a309774c3179e3
Razz Sawhoney <rajsahani1819@gmail.com>
2020-06-30 18:55:59 +0545

    changes

--- mytestwebsite/settings.py
+++ mytestwebsite/settings.py
@@ -277,7 +277,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Email Confs
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
