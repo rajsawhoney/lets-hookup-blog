3baeaaaa8c83ead1499b12fcdd037e21b2260515
Razz Sawhoney <rajsahani1819@gmail.com>
2020-06-30 21:50:58 +0545

    changes

--- mytestwebsite/settings.py
+++ mytestwebsite/settings.py
@@ -276,11 +276,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Email Confs
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.live.com'
-EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp-mail.outlook.com'
 EMAIL_HOST_USER = 'lets-hookup.com@outlook.com'
 EMAIL_HOST_PASSWORD = '*Letshookup123#'
-EMAIL_PORT = 465
+EMAIL_PORT = 25
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
